a61c9bc8a17980e1f21bec3f68da8420d3ef37b3
Amanda-G10 <reahana233@gmail.com>
2020-12-21 08:31:46 -1200

    add oparator

---
+++ oparetor.c
@@ -0,0 +1,33 @@
+//09. Write a C program to recognize different types of operator.
+#include<stdio.h>
+ #include<stdbool.h>
+ int main()
+  {
+   char a[20],b;
+    int i;
+     bool key = true;
+     char *op[]={"+","-","/","*","++","-","=","==","!=","?:",">","<",">=",
+     "<=","!","||","&&","&","%","+=","-=","*=","/=","%=","->", ".","sizeof()",
+     "|","~",">>","<<","^","()","[]","(type)","&=","^=","|=",">>=","<<=",","};
+      printf("Enter $ and space for exit or Enter any word to recognize operator: ");
+       gets(a);
+        b = strlen(a);
+         if(a[0]=='$')
+          exit(0);
+           for(i=0;i<b;i++)
+            {
+            if(strcmp(op[i],a)==0)
+            {
+             printf("Operator.");
+              key = false;
+              break;
+       }
+       }
+        if(key == true)
+         {
+           printf("Not Operator");
+
+
+    }
+    return main();
+}
