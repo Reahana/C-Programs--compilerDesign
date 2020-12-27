4a16dc763d25bc0129d845208f461a488ba7f781
Amanda-G10 <reahana233@gmail.com>
2020-12-27 09:30:00 -1200

    modify number.c

--- number.c
+++ number.c
@@ -3,35 +3,36 @@
 #include<stdbool.h>
 int main()
  {
-     char a[100];
-     int i, b;
-      bool key;
-      printf("\nPlease enter a number or $ for exit: ");
-       gets(a);
-        b=strlen(a);
-         if(a[0]=='$')
-            exit(0);
-         for(i = 0; i<b; i++)
-            {
-                if(a[i]== '.')
+    char a[100];
+    int i, b;
+    bool key;
+    printf("\nPlease enter a number or $ for exit: ");
+    gets(a);
+    b=strlen(a);
+    if(a[0]=='$')
+        exit(0);
+    for(i = 0; i<b; i++)
+        {
+            if(a[i]== '.')
              {
-                  printf("\nIt is floating point number.\n");
-         key = false;
-          break;
-          }
-  else
-    {
-          key = true;
-           }
-             }
+                printf("\nIt is floating point number.\n");
+                key = false;
+                break;
+            }
+            else
+            {
+                key = true;
+            }
+        }
+
              if(key == true)
                 {
-                     printf("\nIt is a integer number.");
-             }
+                     printf("\nIt is a integer number.\n");
+                }
               if(a[0]=='-')
-                    {
-                         printf("\nIt is a negative number.");
-              }
+                {
+                    printf("\nIt is a negative number.\n");
+                }
                else
                     {
                          printf("\nIt is a positive number.");
